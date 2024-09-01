05c41d20fa1cd2a7caec636979c98463496465f2
luanjdes <104906192+luanjdes@users.noreply.github.com>
2024-09-01 16:07:48 -0300

    date001.009

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace meuSistema
+{
+    partial class frmMeuSistema
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txtFrase = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(34, 80);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(454, 80);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtFrase
+            // 
+            this.txtFrase.BackColor = System.Drawing.SystemColors.Window;
+            this.txtFrase.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.txtFrase.Location = new System.Drawing.Point(140, 28);
+            this.txtFrase.Name = "txtFrase";
+            this.txtFrase.Size = new System.Drawing.Size(295, 20);
+            this.txtFrase.TabIndex = 2;
+            this.txtFrase.Text = "Digite seu nome";
+            // 
+            // frmMeuSistema
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtFrase);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnOk);
+            this.Name = "frmMeuSistema";
+            this.Text = "meuSistema";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox txtFrase;
+    }
+}
+
